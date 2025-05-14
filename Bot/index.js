ec6f995716555eeb29bf061f420492e385cdbc80
Basit <muhbasit235@gmail.com>
2025-05-14 11:57:21 +0300

    First commit

---
+++ Bot/index.js
@@ -0,0 +1,32 @@
+const dotenv = require('dotenv');
+const axios = require('axios');
+const Telegram = require('node-telegram-bot-api');
+
+// replace the value below with the Telegram token you receive from @BotFather
+dotenv.config();
+const token = process.env.Telegram_bot_token;
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new Telegram(token);
+
+// Replace these with actual coordinates
+const lat = '24.677058'; // Example: Riyadh latitude
+const lon = '46.823732'; // Example: Riyadh longitude
+const apiKey = process.env.WeatherSecret;
+
+async function getWeather() {
+  try {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
+    );
+    const weatherMessage = `The weather in ${response.data.name} is ${response.data.weather[0].description}, temperature is ${response.data.main.temp}°C, wind speed is ${response.data.wind.speed} m/s, `
+    bot.sendMessage(
+      process.env.Telegram_chat_id,
+      weatherMessage
+    );
+  } catch (err) {
+    console.error('Error fetching weather:', err.message);
+  }
+}
+
+getWeather();
